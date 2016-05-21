ab7f3a310755bd15db085f91b325a41b9656dc8b
Trần Trung <nguyentrantrung1986@gmail.com>
2016-05-21 19:19:45 +0700

    add fragment (temp)

---
+++ LearnFragmentBackStack/app/src/main/java/techkids/mad3/learnfragmentbackstack/Fragment1.java
@@ -0,0 +1,27 @@
+package techkids.mad3.learnfragmentbackstack;
+
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+
+/**
+ * Created by TrungNT on 5/13/2016.
+ */
+public class Fragment1 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_1, container, false);
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+}
--- LearnFragmentBackStack/app/src/main/java/techkids/mad3/learnfragmentbackstack/MainActivity.java
+++ LearnFragmentBackStack/app/src/main/java/techkids/mad3/learnfragmentbackstack/MainActivity.java
@@ -1,5 +1,9 @@
 package techkids.mad3.learnfragmentbackstack;
 
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null) {
+            return;
+        }
+
+        Fragment1 fragment1 = new Fragment1();
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.add(R.id.frgContainer, fragment1);
+
+        ft.commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        FragmentManager fm = getFragmentManager();
+        if (fm.getBackStackEntryCount() > 0) {
+            fm.popBackStack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
